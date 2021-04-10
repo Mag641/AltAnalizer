174ca95588b6beb769735c3e773d067c8bf0c5f5
Mag641 <magistr641@gmail.com>
2021-04-10 17:27:27 +0300

    Initial commit.  - Getting commits history

---
+++ main.py
@@ -0,0 +1,66 @@
+import json
+import re
+
+import requests
+
+USERNAME = 'Mag641'
+TOKEN = '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'
+
+MAIN_URL = 'https://api.github.com'
+ORG_EVENTS_END = '/orgs/{org}/events'
+REPOS_EVENTS_END = '/repos/{owner}/{repo}/events'
+COMMITS_END = '/repos/{}/{}/commits'
+
+eth_repo = 'https://github.com/ethereum/go-ethereum'
+klaytn_reop = 'https://github.com/klaytn/klaytn'
+
+
+def log_error(error):
+    with open('error.txt', 'w+') as file:
+        file.write(json.dumps(error, indent=4))
+
+
+def get_commits_dates(history):
+    dates = list()
+    for obj in history:
+        dates.append(obj['commit']['author']['date'])
+    return dates
+
+
+def main():
+    events = requests.get(
+        MAIN_URL + COMMITS_END.format('klaytn', 'klaytn'),
+    )
+    if events.status_code == 200:
+        events_json = events.json()
+
+        p = re.compile('page=([0-9]+)>; rel="last"')
+        last_page = int(p.search(events.headers['Link']).group(1))
+        print(f'Last page: {last_page}')
+
+        for page in range(2, last_page + 1):
+            print(f'Page: {page}')
+            events = requests.get(
+                MAIN_URL + COMMITS_END.format(), params={'page': page},
+                auth=(USERNAME, TOKEN)
+            )
+            if events.status_code == 200:
+                events_json.extend(events.json())
+            else:
+                log_error(events.json())
+                exit(1)
+    else:
+        log_error(events.json())
+        exit(1)
+
+    commits_dates = get_commits_dates(events.json())
+    with open('klaytn_commits.txt', 'w') as file:
+        file.truncate()
+        file.write('\n'.join(commits_dates))
+
+    with open('klaytn_commits.txt', 'r') as file:
+        commits_dates = file.readlines()
+
+
+if __name__ == '__main__':
+    main()
