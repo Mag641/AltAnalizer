096e8bdc80c09f294fa00cbcb58629cd5f502dd2
Mag641 <magistr641@gmail.com>
2021-04-10 19:12:21 +0300

    Adds function 'get_all', which downloads commits, releases and issues history, grabs their datetimes(not ready) and returns all data, packed in dict. Adds progress bar to get_history func. Changes get_history behaviour to adopt to downloaded data(whether pagination ilinks are included in response header or not).

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-import json
-
-import pandas as pd
-import plotly.graph_objs as go
-from repo_parsing import get_commits_datetimes, get_commits_history
+import repo_parsing
 
 
 def main():
     org = 'klaytn'
     repo = 'klaytn'
-    '''
-    commits_history = get_commits_gistory('klaytn', 'klaytn')
-    commits_dates = get_commits_dates(commits_history)
-    with open(f'{org}_{repo}_commits.txt'.format(org=org, repo=repo), 'w') as file:
-        file.truncate()
-        file.write('\n'.join(commits_dates))
-    '''
 
-    with open('{org}_{repo}_commits.txt'.format(org=org, repo=repo), 'r') as file:
-        commits_dates = file.readlines()
+    whole_klaytn_history = repo_parsing.get_all(org, repo)
+    for key, value in whole_klaytn_history:
+        with open(f'{org}_{repo}_{key}.txt'.format(org=org, repo=repo, key=key), 'w') as file:
+            file.truncate()
+            file.write('\n'.join(value))
 
+
+    #  with open('{org}_{repo}_commits.txt'.format(org=org, repo=repo), 'r') as file:
+    #    commits_dates = file.readlines()
+
+    '''
     series = pd.to_datetime(commits_dates, format='%Y-%m-%dT%H:%M:%S')
     fig = go.Figure(data=go.Scatter(x=series, y=[1] * len(series), mode='markers'))
+    print(1)
+    fig = go.Scatter()
     fig.show()
+    '''
 
 
 if __name__ == '__main__':
--- repo_parsing.py
+++ repo_parsing.py
@@ -4,6 +4,23 @@ import requests
 
 from constants import *
 from utils import log_error
+from tqdm import tqdm
+from pprint import pprint
+
+
+def get_all(org: str, repo: str):
+    print('Getting commits...')
+    commits_history = get_history(org, repo, 'commits')
+    commits_datetimes = get_commits_datetimes(commits_history)
+    print('Getting releases...')
+    releases_history = get_history(org, repo, 'releases')
+    releases_datetimes = get_releases_datetimes(releases_history)
+
+    issues_history = get_history(org, repo, 'issues')
+    issues_datetimes = get_issues_datetimes(issues_history)
+
+    whole_history = {'commits': commits_datetimes, 'releases': releases_datetimes, 'issues': issues_datetimes}
+    return whole_history
 
 
 def get_history(org: str, repo: str, target: str):  # instead of these two use one 'repo_url'?
@@ -15,22 +32,22 @@ def get_history(org: str, repo: str, target: str):  # instead of these two use o
     if events.status_code == 200:
         events_json = events.json()
 
-        p = re.compile('page=([0-9]+)>; rel="last"')
-        last_page = int(p.search(events.headers['Link']).group(1))
-        print(f'Last page: {last_page}')
-
-        for page in range(2, last_page + 1):
-            print(f'Page: {page}')
-            events = requests.get(
-                url,
-                params={'page': page},
-                auth=AUTH_PARAMS
-            )
-            if events.status_code == 200:
-                events_json.extend(events.json())
-            else:
-                log_error(events.json())
-                exit(1)
+        if 'Link' in events.headers:
+            p = re.compile('page=([0-9]+)>; rel="last"')
+            last_page = int(p.search(events.headers['Link']).group(1))
+            pbar = tqdm(range(2, last_page + 1), smoothing=0)
+            for page in pbar:
+                pbar.set_description(f'Page {page} of {last_page}')
+                events = requests.get(
+                    url,
+                    params={'page': page},
+                    auth=AUTH_PARAMS
+                )
+                if events.status_code == 200:
+                    events_json.extend(events.json())
+                else:
+                    log_error(events.json())
+                    exit(1)
     else:
         log_error(events.json())
         exit(1)
@@ -42,3 +59,11 @@ def get_commits_datetimes(history):
     for obj in history:
         dates.append(obj['commit']['author']['date'])
     return dates
+
+
+def get_releases_datetimes(releases_history):
+    pass
+
+
+def get_issues_datetimes(issues_history):
+    pass
